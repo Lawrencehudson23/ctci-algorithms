6547f775b999926fc25609f15bc0da2f7b0cae3d
Lawrence Liu <Lawrencehudson23@gmail.com>
2020-04-27 20:22:09 -0700

    import math

--- chapter3.py
+++ chapter3.py
@@ -1,3 +1,6 @@
+import math
+
+
 class StackNode:
     def __init__(self):
         self.items = []
@@ -28,60 +31,63 @@ class Stack:
 # (such as an array). The stack supports the following operations: push, pop, peek, and is Empty.
 
 # for element in stack[::-1]:
-    # print element
-    # [3.1: ThreeInOne ] Completed
-
-# def threeInOne(arr):
-#     stack1 = Stack()
-#     stack2 = Stack()
-#     stack3 = Stack()
-#     arrLength = math.ceil( len(arr)/3 )
-
-#     for i in range(len(arr)-1, -1, -1):
-#         if len(arr) > 0:
-#             if stack3.size() < arrLength:
-#                 stack3.push( arr.pop() ) # is it better avoid mutating original array?
-#             elif stack2.size() < arrLength:
-#                 stack2.push( arr.pop() )
-#             else:
-#                 stack1.push( arr.pop() )
-
-#     return [stack1.items, stack2.items, stack3.items]
-
-# arr1 = [1, 2, 3, 6, 7, 8, 4, 5, 9, 10, 11, 13, 17, -1, 55]
-# print(threeInOne(arr1))
-
-    def sort(self):
-        temp = Stack()
-        final = Stack()
-        max = self.peek()
-        unSortedCount = self.size()
-        while unSortedCount > 0:
-            while self.size() > 0:
-                if self.peek() > max:
-                    max = self.peek()
-                temp.push(self.pop())
-            while temp.size() > 0:
-                if temp.peek() == max:
-                    unSortedCount -= 1
-                    final.push(temp.pop())
-                    # print(final.items)
-                else:
-                    self.push(temp.pop())
-            if not self.isEmpty():
-                max = self.peek()
-        return final.items
+#     print element
+#     [3.1: ThreeInOne] Completed
 
 
-newStack = Stack()
-newStack.push(8)
-newStack.push(5)
-newStack.push(7)
-newStack.push(1)
+def threeInOne(arr):
+    stack1 = Stack()
+    stack2 = Stack()
+    stack3 = Stack()
+    arrLength = math.ceil(len(arr)/3)
 
-# print(newStack.size())
-# print(newStack.size())
-# newStack.sort()
-# print("is empty", newStack.isEmpty())
-# print("self size", newStack.size())
-print(newStack.sort())
+    for i in range(len(arr)-1, -1, -1):
+        if len(arr) > 0:
+            if stack3.size() < arrLength:
+                # is it better avoid mutating original array?
+                stack3.push(arr.pop())
+            elif stack2.size() < arrLength:
+                stack2.push(arr.pop())
+            else:
+                stack1.push(arr.pop())
+
+    return [stack1.items, stack2.items, stack3.items]
+
+
+arr1 = [1, 2, 3, 6, 7, 8, 4, 5, 9, 10, 11, 13, 17, -1]
+print(threeInOne(arr1))
+
+#     def sort(self):
+#         temp = Stack()
+#         final = Stack()
+#         max = self.peek()
+#         unSortedCount = self.size()
+#         while unSortedCount > 0:
+#             while self.size() > 0:
+#                 if self.peek() > max:
+#                     max = self.peek()
+#                 temp.push(self.pop())
+#             while temp.size() > 0:
+#                 if temp.peek() == max:
+#                     unSortedCount -= 1
+#                     final.push(temp.pop())
+#                     # print(final.items)
+#                 else:
+#                     self.push(temp.pop())
+#             if not self.isEmpty():
+#                 max = self.peek()
+#         return final.items
+
+
+# newStack = Stack()
+# newStack.push(8)
+# newStack.push(5)
+# newStack.push(7)
+# newStack.push(1)
+
+# # print(newStack.size())
+# # print(newStack.size())
+# # newStack.sort()
+# # print("is empty", newStack.isEmpty())
+# # print("self size", newStack.size())
+# print(newStack.sort())
